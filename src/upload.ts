e0413146c45cf278a30a9d136288b69fd3cfb7c3
cyber'T <nailuogg@gmail.com>
2022-09-23 07:43:31 +0800

    Update upload.ts

--- src/upload.ts
+++ src/upload.ts
@@ -5,7 +5,7 @@ import pAll from 'p-all';
 import pRetry from 'p-retry';
 
 function normalizePath(input: string): string {
-  return input.replace(/^\//, '');
+  return input.replace(/^\//, '').split(path.sep).join('/');
 }
 
 export function upload(
